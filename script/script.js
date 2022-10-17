bf2e17487c48e04a13adbb5f77652453defe0898
Юрий Стецкий <yuraha15@yandex.ru>
2022-10-17 17:29:07 +0300

    js menu added

---
+++ script/script.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded",() => {
+   const headerMenuButton = document.querySelector("#headerMenuButton");
+   const headerBurger = document.querySelector(".header__burger-button");
+   // зачем мне дополнительная переменная на один и тот же елемент?
+   const headerMenu = document.querySelector(".header__menu");
+   const headerLink = document.querySelectorAll(".header__link")
+
+   headerMenuButton.addEventListener('click', function () {
+      this.classList.toggle('active');
+      headerMenu.classList.toggle('active');
+   })
+ 
+   for ( let i= 0; i < headerLink.length; i++) {
+      headerLink[i].addEventListener('click', function() {
+         headerMenu.classList.remove('active');
+      })
+   }
+   document.addEventListener('click', function(e){
+        if(headerMenuButton !== e.target){
+            headerMenu.classList.remove("active");
+        }
+    });
+});
+
+
+
