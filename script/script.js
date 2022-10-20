4c8a5340d40cc80cae3454dca840d40a6610d901
Юрий Стецкий <yuraha15@yandex.ru>
2022-10-20 10:25:56 +0300

    feat:button-up now is hidden when burger menu is opened

--- script/script.js
+++ script/script.js
@@ -4,6 +4,8 @@ document.addEventListener("DOMContentLoaded",() => {
    const headerLink = document.querySelectorAll(".header__link");
    const headerLogo = document.querySelector(".header__logo");
    const body = document.querySelector('.no-js');
+   const buttonUp = document.querySelector('.button-up');
+
 
    if(body.classList.contains('no-js')) {
     body.classList.remove('no-js');
@@ -13,12 +15,16 @@ document.addEventListener("DOMContentLoaded",() => {
       this.classList.toggle('active');
       headerMenu.classList.toggle('active');
       headerLogo.classList.toggle('active');
+      body.classList.toggle('overflow-hidden');
+      buttonUp.toggleAttribute('hidden');
    })
 
    for ( let i= 0; i < headerLink.length; i++) {
       headerLink[i].addEventListener('click', function() {
          headerMenu.classList.remove('active');
          headerBurger.classList.remove('active');
+          body.classList.remove('overflow-hidden');
+          buttonUp.removeAttribute('hidden');
       })
    }
 });
