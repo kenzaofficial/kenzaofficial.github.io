d602d0da4dc22663d0a465adbd2b01decd33bd53
Юрий Стецкий <yuraha15@yandex.ru>
2022-10-20 09:54:23 +0300

    feat:now menu is always opened on mobile is there is no js script

--- script/script.js
+++ script/script.js
@@ -1,12 +1,15 @@
 document.addEventListener("DOMContentLoaded",() => {
-   const headerMenuButton = document.querySelector("#headerMenuButton");
    const headerBurger = document.querySelector(".header__burger-button");
-   // зачем мне дополнительная переменная на один и тот же елемент?
    const headerMenu = document.querySelector(".header__menu");
    const headerLink = document.querySelectorAll(".header__link");
    const headerLogo = document.querySelector(".header__logo");
+   const body = document.querySelector('.no-js');
 
-   headerMenuButton.addEventListener('click', function () {
+   if(body.classList.contains('no-js')) {
+    body.classList.remove('no-js');
+   }
+
+   headerBurger.addEventListener('click', function () {
       this.classList.toggle('active');
       headerMenu.classList.toggle('active');
       headerLogo.classList.toggle('active');
