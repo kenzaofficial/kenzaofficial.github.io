792c7379fe0604c31cc0d3acae9492b08129af56
Юрий Стецкий <yuraha15@yandex.ru>
2022-10-20 12:36:54 +0300

    added prettier to js, added wrapper for the whole app

--- script/script.js
+++ script/script.js
@@ -1,29 +1,25 @@
-document.addEventListener("DOMContentLoaded",() => {
-   const headerBurger = document.querySelector(".header__burger-button");
-   const headerLink = document.querySelectorAll(".header__link");
-   const body = document.querySelector('.no-js');
-   const buttonUp = document.querySelector('.button-up');
-   const headerBody = document.querySelector('.header__body');
+document.addEventListener('DOMContentLoaded', () => {
+  const headerBurger = document.querySelector('.header__burger-button');
+  const headerLink = document.querySelectorAll('.header__link');
+  const body = document.querySelector('.no-js');
+  const buttonUp = document.querySelector('.button-up');
+  const headerBody = document.querySelector('.header__body');
 
-
-   if(body.classList.contains('no-js')) {
+  if (body.classList.contains('no-js')) {
     body.classList.remove('no-js');
-   }
+  }
 
-   headerBurger.addEventListener('click', function () {
-    headerBody.classList.toggle('header-menu--active')
-      body.classList.toggle('scroll-lock');
-      buttonUp.toggleAttribute('hidden');
-   })
+  headerBurger.addEventListener('click', function () {
+    headerBody.classList.toggle('header-menu--active');
+    body.classList.toggle('scroll-lock');
+    buttonUp.toggleAttribute('hidden');
+  });
 
-   for ( let i= 0; i < headerLink.length; i++) {
-      headerLink[i].addEventListener('click', function() {
-        headerBody.classList.remove('header-menu--active')
-          body.classList.remove('scroll-lock');
-          buttonUp.removeAttribute('hidden');
-      })
-   }
+  for (let i = 0; i < headerLink.length; i++) {
+    headerLink[i].addEventListener('click', function () {
+      headerBody.classList.remove('header-menu--active');
+      body.classList.remove('scroll-lock');
+      buttonUp.removeAttribute('hidden');
+    });
+  }
 });
-
-
-
